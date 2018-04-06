c69db3624f870525485fddaefb6eac34c021399c
Shawn Koski <shawnnkoski@gmail.com>
2018-04-06 00:00:17 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,37 @@
 # Write your code here.
+def dictionary
+  dictionary = {
+    "hello" => "hi",
+    "to" => "2",
+    "two" => "2",
+    "too" => "2",
+    "for" => "4",
+    "four" => "4",
+    "be" => "b",
+    "you" => "u",
+    "at" => "@",
+    "and" => "&"
+  }
+end
+
+def word_substituter(string)
+  tweet_array = string.split
+  dic_keys = dictionary.keys
+  #puts dic_keys
+  tweet_array.each do |word|
+    puts word
+    dic_keys.each do |key|
+      
+      if word == key
+        puts "HMMMM"
+        puts dictionary[key]
+        word = dictionary[key]
+        puts word
+        puts tweet_array
+      end
+    end
+  end
+  puts tweet_array
+end
+
+word_substituter("hello there guys")
