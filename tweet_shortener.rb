7bde7a39db5c90693eafe6d442e6868f4cd33822
Shawn Koski <shawnnkoski@gmail.com>
2018-04-06 00:07:59 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -24,7 +24,7 @@ def word_substituter(string)
       
       if word == key
         #puts dictionary[key]
-        word = dictionary[key]
+        dic_keys[word] = dictionary[key]
         puts word
         #puts tweet_array
       end
